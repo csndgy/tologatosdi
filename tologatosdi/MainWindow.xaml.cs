9c456f24097d11907a86f7e178f86e0c1276de11
gyarmati.csanad <gyarmati.csanad@58-05>
2024-04-17 09:55:04 +0200

    tologathato

--- tologatosdi/MainWindow.xaml.cs
+++ tologatosdi/MainWindow.xaml.cs
@@ -16,20 +16,21 @@ namespace tologatosdi
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Button> gombok = new List<Button>();
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+
             grdPanel.ColumnDefinitions.Clear();
             grdPanel.RowDefinitions.Clear();
 
-            
+
             for (int oszlopIndex = 0; oszlopIndex < 3; oszlopIndex++)
             {
                 ColumnDefinition ujOszlop = new ColumnDefinition();
@@ -42,29 +43,56 @@ namespace tologatosdi
                 grdPanel.RowDefinitions.Add(ujSor);
             }
 
-            
-            List<Button> gombok = new List<Button>();
-            int szam = 8;
+
+            int szam = 9;
             for (int i = 0; i < szam; i++)
             {
-                Button button = new Button();
-                button.Content = i + 1;
-                gombok.Add(button);
+                if (i == 8)
+                {
+                    Button hidden = new();
+                    hidden.Visibility = Visibility.Hidden;
+                    gombok.Add(hidden);
+                    break;
+                }
+                else
+                {
+
+                    Button button = new Button();
+                    button.Content = i + 1;
+                    button.Click += Button_Click1;
+                    gombok.Add(button);
+
+                }
             }
             Keveres(gombok);
 
-            
+
             for (int i = 0; i < szam; i++)
             {
                 grdPanel.Children.Add(gombok[i]);
-                Grid.SetColumn(gombok[i], i % 3);  
-                Grid.SetRow(gombok[i], i / 3);  
+                Grid.SetColumn(gombok[i], i % 3);
+                Grid.SetRow(gombok[i], i / 3);
             }
 
-            
+
         }
 
-        
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Button gomb = (Button)sender;
+            foreach (var item in gombok)
+            {
+                if (item.Visibility==Visibility.Hidden)
+                {
+                    item.Visibility = Visibility.Visible;
+                    item.Content = gomb.Content; item.Click += Button_Click1;
+                }
+            }
+
+            gomb.Visibility = Visibility.Hidden;
+            gomb.Content = string.Empty;
+        }
+
         private static void Keveres<T>(List<T> lista)
         {
             Random rng = new Random();
@@ -79,5 +107,5 @@ namespace tologatosdi
             }
         }
     }
-    
+
 }
